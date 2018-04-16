6d146930884b6bbab15fbeaea06e66cf9a76f073
Milzan Malik <milzan_malik@yahoo.co.id>
2018-04-16 11:57:05 +0700

    menu design

---
+++ POS Application/POS Application/koneksi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_Application
+{
+    class koneksi
+    {
+        public SqlConnection dbkoneksi;
+        public SqlCommand perintah;
+        public SqlDataAdapter da;
+        public DataTable dt;
+        public DataSet ds;
+        public String db = @"server=MILZAN-PC\SQLEXPRESS;database=ProjectPengolahanNilai;trusted_connection=true";
+        public String sql;
+
+        public void setdt()
+        {
+            dbkoneksi = new SqlConnection(db);
+            perintah = new SqlCommand(sql, dbkoneksi);
+            dt = new DataTable();
+            da = new SqlDataAdapter(perintah);
+            da.Fill(dt);
+        }
+
+        public void setds()
+        {
+            dbkoneksi = new SqlConnection(db);
+            perintah = new SqlCommand(sql, dbkoneksi);
+            ds = new DataSet();
+            da = new SqlDataAdapter(perintah);
+            da.Fill(ds, "tabel");
+
+        }
+
+        public void open()
+        {
+            dbkoneksi = new SqlConnection(db);
+            dbkoneksi.Open();
+        }
+        public void close()
+        {
+            dbkoneksi = new SqlConnection(db);
+            dbkoneksi.Close();
+        }
+    }
+}
+
