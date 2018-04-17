b3fd2b75d31e2310c14757c8da1837333ba61a34
subhan4subhan <ridhosubhan03@gmail.com>
2018-04-17 12:58:13 +0700

    a

--- POS Application/POS Application/FormBarang.xaml.cs
+++ POS Application/POS Application/FormBarang.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MaterialDesignColors.WpfExample.Domain;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,64 @@ namespace POS_Application
         public FormBarang()
         {
             InitializeComponent();
+        }
+
+        koneksi k = new koneksi();
+        private String alamat_foto;
+
+        public void showdata()
+        {
+            k.sql = "select id_barang from tb_barang";
+            k.setdt();
+            dg_barang.ItemsSource = k.dt.DefaultView;
+
+        }
+
+        public void bersih()
+        {
+            txt_hargabarang.Text = "";
+            txt_kdbarang.Text = "";
+            txt_merk.Text = "";
+            txt_namabarang.Text = "";
+            txt_stok.Text = "";
+            cmb_kategori.Text="";
+            dg_barang.UnselectAll();
+        }
+
+        private void btn_simpan_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_kdbarang.Text == "" || txt_namabarang.Text == "" || txt_merk.Text == "" || cmb_kategori.Text == "" || txt_hargabarang.Text == "" || txt_stok.Text == "")
+            {
+                var sampleMessageDialog = new SampleMessageDialog
+                {
+                    Message = { Text = "Lengkapi Dulu Data" }
+                };
+                DialogHost.Show(sampleMessageDialog, "MainDialog");
+            }
+            else
+            {
+                try
+                {
+                    k.sql = "insert into tb_barang select '" + txt_kdbarang.Text + "','" + txt_namabarang.Text + "','" + txt_merk.Text + "','" + cmb_kategori.Text + "','" + txt_hargabarang.Text + "','" + txt_stok.Text + "'";
+                    k.setdt();
+
+                    var sampleMessageDialog = new SampleMessageDialog
+                    {
+                        Message = { Text = "Data Berhasil Tersimpan" }
+                    };
+                    DialogHost.Show(sampleMessageDialog, "MainDialog");
+                    showdata();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Data Gagal Didaftarkan " + ex);
+                }
+            }
+        }
+
+        private void btn_batal_Click(object sender, RoutedEventArgs e)
+        {
+            bersih();
         }
     }
 }
